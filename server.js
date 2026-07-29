3ac9d08a92a2489b0cfdf77e722fde657c426156
Kintomu <taylorpkeefe@gmail.com>
2026-07-29 18:13:18 -0400

    add media api

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require("express");
+const fs = require("fs/promises");
 const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 3000;
+const mediaFilePath = path.join(__dirname, "data", "media.json");
+
+app.get("/api/media", async (request, response) => {
+  try {
+    const mediaFile = await fs.readFile(mediaFilePath, "utf8");
+    const mediaItems = JSON.parse(mediaFile);
+
+    response.json(mediaItems);
+  } catch (error) {
+    console.error("Unable to read media data:", error);
+    response.status(500).json({ error: "Unable to load the media library." });
+  }
+});
 
 app.use(express.static(path.join(__dirname, "public")));
 
